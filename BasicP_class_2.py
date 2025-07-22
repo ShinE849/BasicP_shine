cc594057ff2b4d60088585defecf74bc8b356159
Student <Student@Train11-3-PC201.control.ad.sit.kmutt.ac.th>
2025-07-22 16:15:57 +0700

    Add BasicP_class2.py and BasicP_class_2_Workshop1.py and BasicP_class_2_Workshop2.py

---
+++ BasicP_class_2.py
@@ -0,0 +1,52 @@
+# x = 10
+# print("ค่าตัวแปร x:", x)
+# y = 20
+# b = False
+# print("ค่าตัวแปร y:", y)
+# print("type ของตัวแปร x และ y:", type(x),type(y))
+# print("type ของตัวแปร b:",type(b))
+# num = "1"
+# print("ค่าของตัวแปร num: ", num)
+# print("Type ของ num: ", type(num))
+# print("หลังจากแปลงค่า: ")
+# print("Type ของ num: ", type(int(num)))
+# mod = 10 % 4
+# print(mod)
+# Ex = 4 ** 3
+# print(Ex)
+# ten = 10
+# one = 1
+# print(ten == one)
+# T = True
+# F = False
+# print( not T or F)
+# num1 = int(input("กรอกตัวเลข1: "))
+# num2 = int(input("กรอกตัวเลข2: "))
+# print("num1 =: ", num1)
+# print("num2 =: ", num2)
+# print("Type ของ num1: ", type(num1))
+# print("Type ของ num2: ", type(num2))
+# print( (num1 == num2)
+
+# x = 10
+# y = 20
+# x = input("กรอกตัวเลขของ x: ")
+# y = input("กรอกตัวเลขของ y: ")
+# if x == y:
+#     print("inside if:", "x == y")
+# elif x > y:
+#     print("inside elif", "x > y")
+# elif x < y:
+#     print("inside elif", "x < y")
+# else:
+#     print("inside else: ","x != y")
+# print ("outside if: ", "Starterpack YAIYAI")
+# T = True
+# F = False
+# print(input("กรอก bool 1:"))
+# print(input("กรอก bool 2:"))
+# if T and F:
+#     print("inside: True")
+# else:
+#     print("inside: False")
+x = (input("กรอกระยะทางที่ท่านต้องการคำนวณ"))
